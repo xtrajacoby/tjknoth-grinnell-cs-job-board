a864c18ae09b1cdea17d906ee41fc10e2a8e6e0a
Nicolas Knoebber <knoebber@gmail.com>
2017-02-20 14:39:14 -0600

    merge changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217195010) do
+ActiveRecord::Schema.define(version: 20170220203409) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
